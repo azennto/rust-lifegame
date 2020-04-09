cc0ff7588e71f85b73db39420f23f0fe2a31cf94
azennto <azenntoyuchi@gmail.com>
2020-04-09 16:41:49 +0900

    diehard

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,22 @@ mod print;
 mod set;
 
 fn main() {
-    let mut game_field = new_field::new_field(40,40);
+    let mut game_field = new_field::new_field(60,60);
     
     game_field[20][20] = true;
+    game_field[20][21] = true;
     game_field[21][20] = true;
-    game_field[21][21] = true;
 
-    game_field[25][21] = true;
-    game_field[26][21] = true;
-    game_field[27][21] = true;
-
-    game_field[26][19] = true;
+    game_field[23][20] = true;
+    game_field[24][20] = true;
+    game_field[24][19] = true;
+    game_field[24][18] = true;
+    game_field[25][19] = true;
 
     loop {
         print::print_field(&game_field);
         game_field = set::set(&game_field);
 
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 3));
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 5));
     }
 }
